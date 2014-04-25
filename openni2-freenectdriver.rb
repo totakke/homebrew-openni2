3bb04edb5d52d03e49dcbff66db7966d3898949e
Toshiki Takeuchi <roimisia@gmail.com>
2014-04-25 19:26:37 +0900

    Fix #8

--- openni2-freenectdriver.rb
+++ openni2-freenectdriver.rb
@@ -1,35 +1,42 @@
 require 'formula'
 
 class Openni2Freenectdriver < Formula
-  homepage 'https://github.com/piedar/OpenNI2-FreenectDriver'
-  url 'https://github.com/piedar/OpenNI2-FreenectDriver.git', :revision => 'ee732fd66f83300cad6cfc71b4dcfc2427024b98'
-  version '1.2.1'
-  sha1 'ee732fd66f83300cad6cfc71b4dcfc2427024b98'
-  head 'https://github.com/piedar/OpenNI2-FreenectDriver.git'
+  homepage 'https://github.com/OpenKinect/libfreenect'
+  url 'https://github.com/OpenKinect/libfreenect/archive/v0.4.1.tar.gz'
+  sha1 'a72bf3d60a859fb5b54b30d6e5d52c8359c07888'
+  head 'https://github.com/OpenKinect/libfreenect.git'
 
   option :universal
 
-  depends_on :python
+  depends_on 'cmake' => :build
   depends_on 'openni2' => (build.universal?) ? ['universal'] : []
-  depends_on 'libfreenect' => (build.universal?) ? ['universal'] : []
 
   def install
-    inreplace 'wscript', 'extern/OpenNI-Linux-x64-2.2.0.33/Include', "#{HOMEBREW_PREFIX}/include/ni2"
-    inreplace 'wscript', '/usr/local/include/libfreenect', "#{HOMEBREW_PREFIX}/include/libfreenect"
+    inreplace 'OpenNI2-FreenectDriver/CmakeLists.txt',
+              'extern/OpenNI-Linux-x64-2.2.0.33/Include',
+              "#{HOMEBREW_PREFIX}/include/ni2"
 
     if build.universal?
-        inreplace 'wscript', "conf.env.CXXFLAGS = ['-O2'] #+ strict", "conf.env.CXXFLAGS = ['-O2'] #+ strict\n\tconf.env.append_value('CXXFLAGS', ['-arch', 'i386'])\n\tconf.env.append_value('CXXFLAGS', ['-arch', 'x86_64'])\n\tconf.env.append_value('LINKFLAGS', ['-arch', 'i386'])\n\tconf.env.append_value('LINKFLAGS', ['-arch', 'x86_64'])"
+      ENV.universal_binary
+      ENV['CMAKE_OSX_ARCHITECTURES'] = Hardware::CPU.universal_archs.as_cmake_arch_flags
     end
 
-    system 'python', 'waf', 'configure', 'build'
+    mkdir 'build'
+    cd 'build'
+    system 'cmake', '..', '-DBUILD_OPENNI2_DRIVER=ON'
+    system 'make'
 
+    cd 'lib/OpenNI2-FreenectDriver'
     driver = 'libFreenectDriver.dylib'
-    prefix.install 'build/' + driver
+    while File.symlink?(driver) do
+      driver = File.readlink(driver)
+    end
+    prefix.install driver
 
     src = "#{prefix}/" + driver
     openni2_cellar = Dir.glob("#{HOMEBREW_PREFIX}/Cellar/openni2/*")[0]
-    ln_s src, openni2_cellar + '/lib/ni2/OpenNI2/Drivers/' + driver, :force => true
-    ln_s src, openni2_cellar + '/share/openni2/tools/OpenNI2/Drivers/' + driver, :force => true
-    ln_s src, openni2_cellar + '/share/openni2/samples/Bin/OpenNI2/Drivers/' + driver, :force => true
+    ln_s src, openni2_cellar + '/lib/ni2/OpenNI2/Drivers/libFreenectDriver.dylib', :force => true
+    ln_s src, openni2_cellar + '/share/openni2/tools/OpenNI2/Drivers/libFreenectDriver.dylib', :force => true
+    ln_s src, openni2_cellar + '/share/openni2/samples/Bin/OpenNI2/Drivers/libFreenectDriver.dylib', :force => true
   end
 end
